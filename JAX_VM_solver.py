2d9f8097edd0446f2c19875a7a989036b2411176
csvega <fakeemail@fake.com>
2024-08-13 14:07:47 -0500

    Improved formula for plasma energy.

--- JAX_VM_solver.py
+++ JAX_VM_solver.py
@@ -391,45 +391,40 @@ def anti_transform(Ck, Fk, alpha_s, u_s, Lx, Ly, Lz, Nx, Ny, Nz, Nvx, Nvy, Nvz, 
     Ce = C[:, :(Nn * Nm * Np), ...]
     Ci = C[:, (Nn * Nm * Np):, ...]
     
-    x = jnp.linspace(0, Lx, Nx)
-    y = jnp.linspace(0, Ly, Ny)
-    z = jnp.linspace(0, Lz, Nz)
-    vx = jnp.linspace(-5, 5, Nvx)
-    vy = jnp.linspace(-5, 5, Nvy)
-    vz = jnp.linspace(-5, 5, Nvz)
-    X, Y, Z, Vx, Vy, Vz = jnp.meshgrid(x, y, z, vx, vy, vz, indexing='ij')
+    # x = jnp.linspace(0, Lx, Nx)
+    # y = jnp.linspace(0, Ly, Ny)
+    # z = jnp.linspace(0, Lz, Nz)
+    # vx = jnp.linspace(-5, 5, Nvx)
+    # vy = jnp.linspace(-5, 5, Nvy)
+    # vz = jnp.linspace(-5, 5, Nvz)
+    # X, Y, Z, Vx, Vy, Vz = jnp.meshgrid(x, y, z, vx, vy, vz, indexing='ij')
     
-    xi_x = (Vx - u_s[0]) / alpha_s[0]
-    xi_y = (Vy - u_s[1]) / alpha_s[1]
-    xi_z = (Vz - u_s[2]) / alpha_s[2]
+    # xi_x = (Vx - u_s[0]) / alpha_s[0]
+    # xi_y = (Vy - u_s[1]) / alpha_s[1]
+    # xi_z = (Vz - u_s[2]) / alpha_s[2]
     
-    full_Hermite_basis_e = jax.vmap(generate_Hermite_basis, in_axes=(None, None, None, None, None, None, 0))(xi_x, xi_y, xi_z, Nn, Nm, Np, jnp.arange(Nn * Nm * Np))
+    # full_Hermite_basis_e = jax.vmap(generate_Hermite_basis, in_axes=(None, None, None, None, None, None, 0))(xi_x, xi_y, xi_z, Nn, Nm, Np, jnp.arange(Nn * Nm * Np))
     
-    xi_x = (Vx - u_s[3]) / alpha_s[3]
-    xi_y = (Vy - u_s[4]) / alpha_s[4]
-    xi_z = (Vz - u_s[5]) / alpha_s[5]
+    # xi_x = (Vx - u_s[3]) / alpha_s[3]
+    # xi_y = (Vy - u_s[4]) / alpha_s[4]
+    # xi_z = (Vz - u_s[5]) / alpha_s[5]
     
-    full_Hermite_basis_i = jax.vmap(generate_Hermite_basis, in_axes=(None, None, None, None, None, None, 0))(xi_x, xi_y, xi_z, Nn, Nm, Np, jnp.arange(Nn * Nm * Np))
+    # full_Hermite_basis_i = jax.vmap(generate_Hermite_basis, in_axes=(None, None, None, None, None, None, 0))(xi_x, xi_y, xi_z, Nn, Nm, Np, jnp.arange(Nn * Nm * Np))
     
-    shape_C_expanded = Ce.shape + (Nvx, Nvy, Nvz)
+    # shape_C_expanded = Ce.shape + (Nvx, Nvy, Nvz)
     
-    Ce_expanded = jnp.expand_dims(Ce, (4, 5, 6))
-    Ci_expanded = jnp.expand_dims(Ci, (4, 5, 6))
+    # Ce_expanded = jnp.expand_dims(Ce, (4, 5, 6))
+    # Ci_expanded = jnp.expand_dims(Ci, (4, 5, 6))
     
-    Ce_expanded = jnp.broadcast_to(Ce_expanded, shape_C_expanded)
-    Ci_expanded = jnp.broadcast_to(Ci_expanded, shape_C_expanded)
+    # Ce_expanded = jnp.broadcast_to(Ce_expanded, shape_C_expanded)
+    # Ci_expanded = jnp.broadcast_to(Ci_expanded, shape_C_expanded)
     
-    fe = jnp.array([jnp.sum(Ce_expanded[i, ...] * full_Hermite_basis_e, axis=0) for i in jnp.arange(Ce.shape[0])])
-    fi = jnp.array([jnp.sum(Ci_expanded[i, ...] * full_Hermite_basis_i, axis=0) for i in jnp.arange(Ce.shape[0])])
+    # fe = jnp.array([jnp.sum(Ce_expanded[i, ...] * full_Hermite_basis_e, axis=0) for i in jnp.arange(Ce.shape[0])])
+    # fi = jnp.array([jnp.sum(Ci_expanded[i, ...] * full_Hermite_basis_i, axis=0) for i in jnp.arange(Ce.shape[0])])
     
-    electron_energy_dens = 0.5 * (trapezoid(trapezoid(trapezoid(
-                fe * (Vx ** 2 + Vy ** 2 + Vz ** 2),
-                vx, axis=-3), vy, axis=-2), vz, axis=-1))
-    
-    ion_energy_dens = 0.5 * (trapezoid(trapezoid(trapezoid(
-                fi * (Vx ** 2 + Vy ** 2 + Vz ** 2),
-                vx, axis=-3), vy, axis=-2), vz, axis=-1))
-    
+    electron_energy_dens = 0.5 * ((3 / 2) * Ce[:, 0, ...] + Ce[:, 2, ...] + Ce[:, Nn + 2, ...] + Ce[:, Nn * Nm + 2, ...])
+    ion_energy_dens = 0.5 * ((3 / 2) * Ci[:, 0, ...] + Ci[:, 2, ...] + Ci[:, Nn + 2, ...] + Ci[:, Nn * Nm + 2, ...])
+     
     plasma_energy = jnp.mean(electron_energy_dens, axis=[1, 2, 3]) + jnp.mean(ion_energy_dens, axis=[1, 2, 3])
     
     EM_energy = jnp.mean((E ** 2 + B ** 2) / 2, axis=[1, 2, 3, 4])
