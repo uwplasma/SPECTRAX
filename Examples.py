Fixed energy formula (again). Added new examples.

Examples.py
@@ -137,7 +137,7 @@ def Landau_damping_1D(Lx, Omega_ce, mi_me):
     
     kx = 2 * jnp.pi / Lx # Wavenumber.
     
-    dn = 0.1 # Density fluctuation
+    dn = 0.01 # Density fluctuation.
     
     # Magnetic and electric fields.
     B = lambda x, y, z: jnp.array([Omega_ce * jnp.ones_like(x), jnp.zeros_like(y), jnp.zeros_like(z)])
@@ -151,3 +151,31 @@ def Landau_damping_1D(Lx, Omega_ce, mi_me):
                                         jnp.exp(-(vx ** 2 + vy ** 2 + vz ** 2) / (2 * vti ** 2))))
     
     return B, E, fe, fi
+
+
+def Landau_damping_HF_1D(Lx, Omega_ce, alpha_e, alpha_i, Nn):
+    """
+    I have to add docstrings!
+    """
+    
+    vte = alpha_e / jnp.sqrt(2) # Electron thermal velocity.
+    vti = alpha_i / jnp.sqrt(2) # Ion thermal velocity.
+    
+    kx = 2 * jnp.pi / Lx # Wavenumber.
+    
+    dn = 0.01 # Density fluctuation.
+    
+    # Fourier components of magnetic and electric fields.
+    Fk_0 = jnp.array([[[[dn / (2 * kx) + 0 * 1j]], [[0 + 0 * 1j]], [[dn / (2 * kx) + 0 * 1j]]], [[[0 + 0 * 1j]], [[0 + 0 * 1j]], [[0 + 0 * 1j]]], [[[0 + 0 * 1j]], [[0 + 0 * 1j]], [[0 + 0 * 1j]]], 
+                      [[[0 + 0 * 1j]], [[Omega_ce + 0 * 1j]], [[0 + 0 * 1j]]], [[[0 + 0 * 1j]], [[0 + 0 * 1j]], [[0 + 0 * 1j]]], [[[0 + 0 * 1j]], [[0 + 0 * 1j]], [[0 + 0 * 1j]]]])
+    
+    # Hermite-Fourier components of electron and ion distribution functions.
+    Ce0_mk, Ce0_0, Ce0_k = 0 + 1j * (1 / 2) * (1 / vte ** 3) * dn, (1 / vte ** 3) + 0 * 1j, 0 - 1j * (1 / 2) * (1 / vte ** 3) * dn
+    Ci0_0 = (1 / vti ** 3) + 0 * 1j
+    Ck_0 = jnp.zeros((2 * Nn, 3, 1, 1), dtype=jnp.complex128)
+    Ck_0 = Ck_0.at[0, 0, 0, 0].set(Ce0_mk)
+    Ck_0 = Ck_0.at[0, 1, 0, 0].set(Ce0_0)
+    Ck_0 = Ck_0.at[0, 2, 0, 0].set(Ce0_k)
+    Ck_0 = Ck_0.at[Nn, 1, 0, 0].set(Ci0_0)
+    
+    return Ck_0, Fk_0