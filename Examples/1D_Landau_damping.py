11e79c34cd5ed5377110fbf6f5461de2a31bbace
OhFact <87435414+OhFact@users.noreply.github.com>
2025-05-26 16:04:01 -0500

    Update 1D_Landau_damping.py to Variable Species Modes

--- Examples/1D_Landau_damping.py
+++ Examples/1D_Landau_damping.py
@@ -1,15 +1,13 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-number_of_processors_to_use = 5 # Parallelization, this should divide total resolution
+number_of_processors_to_use = 5
 os.environ["XLA_FLAGS"] = f'--xla_force_host_platform_device_count={number_of_processors_to_use}'
 from time import time
 import jax.numpy as jnp
 from jax import block_until_ready
 from spectrax import simulation, load_parameters, plot
 
-# Read from input.toml
-# input_parameters, solver_parameters = load_parameters('input_1D_landau_damping.toml')
 toml_file = os.path.join(os.path.dirname(__file__), 'input_1D_landau_damping.toml')
 input_parameters, solver_parameters = load_parameters(toml_file)
 
@@ -20,6 +18,7 @@ Omega_cs = input_parameters["Omega_cs"]
 dn = input_parameters["dn1"]
 Nx = solver_parameters["Nx"]
 Nn = solver_parameters["Nn"]
+Ns = solver_parameters["Ns"]
 
 # Fourier components of magnetic and electric fields.
 Fk_0 = jnp.zeros((6, 1, Nx, 1), dtype=jnp.complex128)
@@ -27,14 +26,23 @@ Fk_0 = Fk_0.at[0, 0, int((Nx-1)/2-nx), 0].set(dn * Lx / (4 * jnp.pi * nx * Omega
 Fk_0 = Fk_0.at[0, 0, int((Nx-1)/2+nx), 0].set(dn * Lx / (4 * jnp.pi * nx * Omega_cs[0]))
 input_parameters["Fk_0"] = Fk_0
 
+ncps_1d = jnp.array(Nn[:Ns])
+total_c = jnp.sum(ncps_1d)
+offsets = jnp.cumsum(jnp.concatenate([jnp.array([0]), ncps_1d[:-1]]))
+
 # Hermite-Fourier components of electron and ion distribution functions.
-Ce0_mk, Ce0_0, Ce0_k = 0 + 1j * (1 / (2 * alpha_s[0] ** 3)) * dn, 1 / (alpha_s[0] ** 3) + 0 * 1j, 0 - 1j * (1 / (2 * alpha_s[0] ** 3)) * dn
+Ce0_mk = 0 + 1j * (1 / (2 * alpha_s[0] ** 3)) * dn
+Ce0_0 = 1 / (alpha_s[0] ** 3) + 0 * 1j
+Ce0_k = 0 - 1j * (1 / (2 * alpha_s[0] ** 3)) * dn
 Ci0_0 = 1 / (alpha_s[3] ** 3) + 0 * 1j
-Ck_0 = jnp.zeros((2 * Nn, 1, Nx, 1), dtype=jnp.complex128)
-Ck_0 = Ck_0.at[0, 0, int((Nx-1)/2-nx), 0].set(Ce0_mk)
-Ck_0 = Ck_0.at[0, 0, int((Nx-1)/2), 0].set(Ce0_0)
-Ck_0 = Ck_0.at[0, 0, int((Nx-1)/2+nx), 0].set(Ce0_k)
-Ck_0 = Ck_0.at[Nn, 0, int((Nx-1)/2), 0].set(Ci0_0)
+Ck_0 = jnp.zeros((total_c, 1, Nx, 1), dtype=jnp.complex128)
+electron_offset = offsets[0]
+Ck_0 = Ck_0.at[electron_offset, 0, int((Nx-1)/2-1), 0].set(Ce0_mk)
+Ck_0 = Ck_0.at[electron_offset, 0, int((Nx-1)/2), 0].set(Ce0_0)
+Ck_0 = Ck_0.at[electron_offset, 0, int((Nx-1)/2+1), 0].set(Ce0_k)
+ion_offset = offsets[1]
+Ck_0 = Ck_0.at[ion_offset, 0, int((Nx-1)/2), 0].set(Ci0_0)
+
 input_parameters["Ck_0"] = Ck_0
 
 # Simulate
